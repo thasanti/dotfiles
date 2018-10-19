ac8ea365daa28c9bfdc6fdee375b50603b5693b8
Lukas Santner <thasanti1337@gmail.com>
2018-10-19 14:49:43 +0200

    added wifiFix script

---
+++ .config/wifiFix.sh
@@ -0,0 +1,7 @@
+wget https://github.com/kvalo/ath10k-firmware/raw/master/QCA6174/hw3.0/board.bin
+sudo mv board.bin /lib/firmware/ath10k/QCA6174/hw3.0/board.bin
+wget https://github.com/kvalo/ath10k-firmware/raw/master/QCA6174/hw3.0/board-2.bin
+sudo mv board-2.bin /lib/firmware/ath10k/QCA6174/hw3.0/board-2.bin
+wget https://github.com/kvalo/ath10k-firmware/raw/master/QCA6174/hw3.0/4.4.1/firmware-6.bin_WLAN.RM.4.4.1-00079-QCARMSWPZ-1
+sudo mv firmware-6.bin_WLAN.RM.4.4.1-00079-QCARMSWPZ-1 /lib/firmware/ath10k/QCA6174/hw3.0/firmware-6.bin
+
